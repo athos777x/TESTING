9619f6df181d3337a0b22fc2343b3e9ec84de7dd
athos777x <159774085+athos777x@users.noreply.github.com>
2024-07-02 22:28:31 +0800

    update

--- src/Pages/EmployeePage.js
+++ src/Pages/EmployeePage.js
@@ -1,13 +1,11 @@
-// EmployeePage.js
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import '../CssPage/EmployeePage.css'; // Make sure to create and link this CSS file
+import SearchFilter from '../Utilities/SearchFilter';
+import '../CssPage/EmployeePage.css';
 
 function EmployeePage() {
   const [employees, setEmployees] = useState([]);
   const [filteredEmployees, setFilteredEmployees] = useState([]);
-  const [positions, setPositions] = useState([]);
-  const [departments, setDepartments] = useState([]);
   const [filters, setFilters] = useState({
     searchTerm: '',
     position: '',
@@ -15,124 +13,86 @@ function EmployeePage() {
   });
 
   useEffect(() => {
-    axios.get('http://localhost:3001/employees')
-      .then(response => {
-        setEmployees(response.data);
-        setFilteredEmployees(response.data);
-        console.log('Fetched employees:', response.data);
-      })
-      .catch(error => {
-        console.error('There was an error fetching the employees!', error);
-      });
-
-    axios.get('http://localhost:3001/employees/positions')
-      .then(response => {
-        setPositions(response.data);
-        console.log('Fetched positions:', response.data);
-      })
-      .catch(error => {
-        console.error('There was an error fetching the positions!', error);
-      });
-
-    axios.get('http://localhost:3001/employees/departments')
-      .then(response => {
-        setDepartments(response.data);
-        console.log('Fetched departments:', response.data);
-      })
-      .catch(error => {
-        console.error('There was an error fetching the departments!', error);
-      });
+    fetchEmployees();
   }, []);
 
+  const fetchEmployees = async (appliedFilters = {}) => {
+    try {
+      const response = await axios.get('http://localhost:3001/employees', {
+        params: appliedFilters
+      });
+      console.log('API response:', response.data); // Log the API response
+      const sortedEmployees = response.data.sort((a, b) => a.lastname.localeCompare(b.lastname));
+      setEmployees(sortedEmployees);
+      setFilteredEmployees(sortedEmployees);
+      console.log('Sorted employees:', sortedEmployees); // Log the sorted employees
+    } catch (error) {
+      console.error('There was an error fetching the employees!', error);
+    }
+  };
+
   const handleSearch = (searchTerm) => {
-    setFilters(prevFilters => ({ ...prevFilters, searchTerm }));
+    setFilters(prevFilters => {
+      const updatedFilters = { ...prevFilters, searchTerm };
+      applyFilters(updatedFilters);
+      return updatedFilters;
+    });
   };
 
   const handleFilterChange = (type, value) => {
-    setFilters(prevFilters => ({ ...prevFilters, [type]: value }));
+    setFilters(prevFilters => {
+      const updatedFilters = { ...prevFilters, [type]: value };
+      return updatedFilters;
+    });
   };
 
-  const handleApplyFilters = () => {
+  const applyFilters = (updatedFilters) => {
     let filtered = employees;
 
-    if (filters.searchTerm) {
+    if (updatedFilters.position) {
+      filtered = filtered.filter(employee => employee.position === updatedFilters.position);
+    }
+    if (updatedFilters.department) {
+      filtered = filtered.filter(employee => employee.department === updatedFilters.department);
+    }
+    if (updatedFilters.searchTerm) {
       filtered = filtered.filter(employee =>
-        employee.name.toLowerCase().includes(filters.searchTerm.toLowerCase())
+        employee.lastname.toLowerCase().includes(updatedFilters.searchTerm.toLowerCase()) ||
+        employee.firstname.toLowerCase().includes(updatedFilters.searchTerm.toLowerCase())
       );
-    }
-    if (filters.position) {
-      filtered = filtered.filter(employee => employee.position === filters.position);
-    }
-    if (filters.department) {
-      filtered = filtered.filter(employee => employee.department === filters.department);
     }
 
     setFilteredEmployees(filtered);
-    console.log('Filtered employees:', filtered);
+    console.log('Filtered employees:', filtered); // Log the filtered employees
+  };
+
+  const handleApplyFilters = () => {
+    console.log('Applying filters:', filters);
+    fetchEmployees(filters);
   };
 
   return (
     <div className="employee-container">
       <h1 className="employee-title">Employees</h1>
-      <div className="search-filter-container">
-        <div className="search-filter">
-          <input
-            type="text"
-            placeholder="Search by name..."
-            value={filters.searchTerm}
-            onChange={(e) => handleSearch(e.target.value)}
-          />
-          <div>
-            <label htmlFor="position">Position:</label>
-            <select
-              id="position"
-              value={filters.position}
-              onChange={(e) => handleFilterChange('position', e.target.value)}
-            >
-              <option value="">All Positions</option>
-              {positions.map(position => (
-                <option key={position} value={position}>{position}</option>
-              ))}
-            </select>
-          </div>
-          <div>
-            <label htmlFor="department">Department:</label>
-            <select
-              id="department"
-              value={filters.department}
-              onChange={(e) => handleFilterChange('department', e.target.value)}
-            >
-              <option value="">All Departments</option>
-              {departments.map(department => (
-                <option key={department} value={department}>{department}</option>
-              ))}
-            </select>
-          </div>
-          <button onClick={handleApplyFilters}>Apply Filters</button>
-        </div>
+      <div className="employee-search-filter-container">
+        <SearchFilter
+          handleSearch={handleSearch}
+          handleFilter={handleFilterChange}
+          handleApplyFilters={handleApplyFilters}
+        />
       </div>
-      <table className="employee-table">
-        <thead>
-          <tr>
-            <th>Name</th>
-            <th>Position</th>
-            <th>Department</th>
-            <th>Email</th>
-            <th>Phone Number</th>
-          </tr>
-        </thead>
-        <tbody>
-          {filteredEmployees.map(employee => (
-            <tr key={employee.employee_id}>
-              <td>{employee.name}</td>
-              <td>{employee.position}</td>
-              <td>{employee.department}</td>
-              <td>{employee.email}</td>
-              <td>{employee.phone_number}</td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
+      <div className="employee-list">
+        {filteredEmployees.map((employee, index) => (
+          <div key={employee.employee_id} className="employee-item-container">
+            <div className="employee-item">
+              <p className="employee-name">
+                {index + 1}. {employee.firstname} {employee.middlename && `${employee.middlename[0]}.`} {employee.lastname}
+              </p>
+              <span className="employee-info">{employee.role_name} - {employee.contact_number}</span>
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
